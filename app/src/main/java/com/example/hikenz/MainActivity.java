d67d6a8144eedef7159ac7d4022d9e6d29497c17
cferigo <48233881+cferigo@users.noreply.github.com>
2020-06-24 17:18:37 +1200

    trying to fix github conflict again

--- app/src/main/java/com/example/hikenz/MainActivity.java
+++ app/src/main/java/com/example/hikenz/MainActivity.java
@@ -5,27 +5,38 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.sax.StartElementListener;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference trackRef = db.collection("Tracks");
     private TrackAdapter adapter;
+
+    Button searchBtn;
+    EditText searchEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +44,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setUpRecyclerView();
+        searchBtn = findViewById(R.id.main_search_button);
+        searchEditText = findViewById(R.id.main_search_editText);
     }
 
     @Override
@@ -78,7 +91,35 @@ public class MainActivity extends AppCompatActivity {
                 //Toast.makeText(getApplicationContext(),"id: " + id, Toast.LENGTH_LONG).show();
             }
         });
+
+        // i got no idea what im doing with the search tracks stuff, but this crashes the app so yea
+        /*searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (searchEditText == null){
+                    Toast.makeText(getApplicationContext(),"Please enter search term" ,Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    db.collection("Tracks")
+                            .whereEqualTo(String.valueOf(searchEditText), true)
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                            Toast.makeText(getApplicationContext(),"search kind of words" ,Toast.LENGTH_SHORT).show();
+                                        }
+                                    } else {
+                                        Toast.makeText(getApplicationContext(),"your project sucks" + searchEditText ,Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });*/
     }
+
 
     @Override
     protected void onStart() {
@@ -97,4 +138,5 @@ public class MainActivity extends AppCompatActivity {
         startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         finish();
     }
+
 }
