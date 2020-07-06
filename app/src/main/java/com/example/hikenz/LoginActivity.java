f006980e4ae4afda8a79c11e1ce018025a40204f
cferigo <48233881+cferigo@users.noreply.github.com>
2020-07-06 12:22:43 +1200

    few updates

--- app/src/main/java/com/example/hikenz/LoginActivity.java
+++ app/src/main/java/com/example/hikenz/LoginActivity.java
@@ -4,11 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Layout;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,6 +22,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    RelativeLayout base;
     EditText mEmail, mPassword;
     Button mLoginBtn;
     TextView mRegisterBtn;
@@ -33,11 +38,19 @@ public class LoginActivity extends AppCompatActivity {
         mLoginBtn = findViewById(R.id.login_Button);
         mRegisterBtn = findViewById(R.id.register_Link);
         fAuth = FirebaseAuth.getInstance();
+        base = findViewById(R.id.login_base);
 
         // This avoids return users needing to login
         if(fAuth.getCurrentUser() != null) {
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
             finish();
+        }
+
+        int orientation = getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            base.setBackgroundResource(R.drawable.gradient_background);
+        } else {
+            base.setBackgroundResource(R.drawable.bg3);
         }
     }
 
